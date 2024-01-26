a93480c4b1e1a16c7ab8bed2fd1b6f878a04b363
Jacques Roux <jacquesf.roux@gmail.com>
2024-01-26 06:58:46 +0200

    Adding a custom router using Router class & adding views

--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
 <?php
 require '../helpers.php';
+require basePath('Router.php');
 
-loadView('home');
+$router = new Router();
+$routes = require basePath('routes.php');
+$uri = $_SERVER['REQUEST_URI'];
+$method = $_SERVER['REQUEST_METHOD'];
+
+$router->route($uri, $method);

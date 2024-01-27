ba8d2289047ce2cc41083ddba8488d28d4767278
Jacques Roux <jacquesf.roux@gmail.com>
2024-01-27 07:26:34 +0200

    Re-structure folders, added composer with PSR-4 autoloading, added namespaces and fix all issues after inplementing composer.

--- Framework/Database.php
+++ Framework/Database.php
@@ -1,5 +1,11 @@
 <?php
 
+namespace Framework;
+
+use PDO;
+use Exception;
+use PDOException;
+
 class Database
 {
   public $conn;
--- public/index.php
+++ public/index.php
@@ -1,7 +1,18 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
 require '../helpers.php';
-require basePath('Router.php');
-require basePath('Database.php');
+
+use Framework\Router;
+
+// require basePath('Framework/Router.php');
+// require basePath('Framework/Database.php');
+
+// spl_autoload_register(function ($class) {
+//   $path = basePath('Framework/' . $class . '.php');
+//   if (file_exists($path)) {
+//     require $path;
+//   }
+// });
 
 // Instantiating the router class
 $router = new Router();

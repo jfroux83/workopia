ba0f1ea81d5e4ba12a0641b714c0f8366d0b714b
Jacques Roux <jacquesf.roux@gmail.com>
2024-01-27 06:45:48 +0200

    Added more controllers and views for Job detail section

--- Database.php
+++ Database.php
@@ -15,7 +15,7 @@ class Database
 
     $options = [
       PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
     ];
 
     try {
@@ -34,10 +34,16 @@ class Database
    * @return PDOStatement
    * @throws PDOException
    */
-  public function query($query)
+  public function query($query, $params = [])
   {
     try {
       $sth = $this->conn->prepare($query);
+
+      // Bind named params
+      foreach ($params as $param => $value) {
+        $sth->bindValue(':' . $param, $value);
+      }
+
       $sth->execute();
       return $sth;
     } catch (PDOException $e) {

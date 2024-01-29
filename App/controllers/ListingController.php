3ca9b3b00eed64c62b4ef41497dd4837ff4a7b20
Jacques Roux <jacquesf.roux@gmail.com>
2024-01-29 16:53:47 +0200

    Adding delete route with method & edit / update route with edit method

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -42,6 +42,7 @@ class ListingController
   /**
    * Show a single listing
    *
+   * @param array $params
    * @return void
    */
   public function show($params)
@@ -122,5 +123,60 @@ class ListingController
 
       redirect('/listings');
     }
+  }
+
+  /**
+   * Delete a listing
+   * 
+   * @param array $params
+   * @return void
+   */
+  public function destroy($params)
+  {
+    $id = $params['id'];
+
+    $params = [
+      'id' => $id
+    ];
+
+    $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+    if (!$listing) {
+      ErrorController::notFound('Listing not found');
+      return;
+    }
+
+    $this->db->query('DELETE FROM listings WHERE id = :id', $params);
+
+    // Set flash message
+    $_SESSION['success_message'] = 'Listing deleted successfully';
+
+    redirect('/listings');
+  }
+
+  /**
+   * Show the listing edit form
+   *
+   * @param array $params
+   * @return void
+   */
+  public function edit($params)
+  {
+    $id = $params['id'] ?? '';
+
+    $params = [
+      'id' => $id
+    ];
+    $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+    // check if listing exists
+    if (!$listing) {
+      ErrorController::notFound('Listing not found');
+      return;
+    }
+
+    loadView('/listings/edit', [
+      'listing' => $listing
+    ]);
   }
 }

3ed013ebf8f27c47b6bb60d663cdfa02e7ce74c5
Jacques Roux <jacquesf.roux@gmail.com>
2024-01-30 16:51:45 +0200

    Store registered user to DB, add Session class and handle logout route with controller method

--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
 <?php
-session_start();
 require __DIR__ . '/../vendor/autoload.php';
-require '../helpers.php';
 
 use Framework\Router;
+use Framework\Session;
+
+Session::start();
+
+require '../helpers.php';
 
 // require basePath('Framework/Router.php');
 // require basePath('Framework/Database.php');
